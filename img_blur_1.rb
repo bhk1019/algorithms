1a47747763fa4a0a47b24004c813d80fa0f7bd51
Ben Kim <bhk1019@gmail.com>
2018-02-14 05:11:48 +0000

    assigned blurred_image_array to a new array - fixed problem of blur doing nothing

--- img_blur_1.rb
+++ img_blur_1.rb
@@ -14,24 +14,30 @@ class Image
   def blur 
   width = self.img_array[0].length
   height = self.img_array.length
-  blurred_image_array = self.img_array
+  blurred_image_array = [[0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]]
 
   height.times do |i|
     width.times do |j|
-      if self.img_array[j] == 1
+      if self.img_array[i][j] == 1
         blurred_image_array[i][j] = 1
-
+        
         if 0 < j
-          blurred_image_array[i][j-1] = 1
+          blurred_image_array[i][j-1] = 1        
         end 
         if j < width - 1
           blurred_image_array[i][j+1] = 1
+          
         end
         if 0 < i 
           blurred_image_array[i-1][j] = 1
+          
         end
         if i < height - 1
           blurred_image_array[i+1][j] = 1
+          
         end
       end
 
